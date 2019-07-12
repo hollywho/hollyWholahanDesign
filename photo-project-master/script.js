90eed320dfdbd32746a9b56f2b3881e33c41c11e
HollyWho <white.holly317@gmail.com>
2019-07-12 13:07:45 -0400

    commit

--- photo-project-master/script.js
+++ photo-project-master/script.js
@@ -1,7 +1,14 @@
 
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,6000);
 
+function nextSlide() {
+    slides[currentSlide].className = 'slide';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = 'slide showing';
+}
 
-
-$(".img-fluid").addClass("wow fadeIn z-depth-1-half");
-
-new WOW().init();
+$(window).scroll(function(){
+  $(".nav-bar").css("opacity", 1 - $(window).scrollTop() / 250);
+});
